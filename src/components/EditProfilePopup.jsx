93cde4e6220b497f9f78205acc1fd00d517510ee
miklekuzichev <dev310105@gmail.com>
2023-08-30 10:07:16 +0300

    Fix: Add useEffect in EditProfilePopup

--- src/components/EditProfilePopup.jsx
+++ src/components/EditProfilePopup.jsx
@@ -1,7 +1,29 @@
+import React, { useContext } from "react";
+import { CurrentUserContext } from '../context/CurrentUserContext.js'
 import PopupWithForm from './PopupWithForm';
 
 function EditProfilePopup(props) {
     
+    const currentUser = React.useContext(CurrentUserContext); // Подписка на контекст
+    const [description, setDescription] = React.useState('');
+    const [name, setName] = React.useState('');
+
+    function handleChange(evt) {
+        setName(evt.target.value);
+        setDescription(evt.target.value);
+      }
+
+
+    // После загрузки текущего пользователя из API
+    // его данные будут использованы в управляемых компонентах.
+    React.useEffect(() => {
+    setName(currentUser.name);
+    setDescription(currentUser.about);
+    }, [currentUser]);
+
+
+
+
     return (
 
         <PopupWithForm 
@@ -11,9 +33,9 @@ function EditProfilePopup(props) {
             title='Редактировать профиль'
             buttonText='Сохранить'
             >
-            <input type='text' name="username" id="name" placeholder="Иван Иванов" minLength="2" maxLength="40" className="popup__input popup__input_type_name" required/>
+            <input type='text' name="username" id="name" value={name} onChange={handleChange} placeholder="Иван Иванов" minLength="2" maxLength="40" className="popup__input popup__input_type_name" required/>
             <span className="popup__error name-error"></span>
-            <input type='text' name="userprofile" id="profile" placeholder="Фотограф" minLength="2" maxLength="200" className="popup__input popup__input_type_profile" required/>
+            <input type='text' name="userprofile" id="profile" value={description} onChange={handleChange} placeholder="Фотограф" minLength="2" maxLength="200" className="popup__input popup__input_type_profile" required/>
             <span className="popup__error profile-error"></span>
         </PopupWithForm>
 
